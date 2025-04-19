9372e81cc5d0977aab430d68c5abd34cb2a41728
Emerson <emerson.day@gmail.com>
2025-04-19 14:50:51 -0700

    Initial Commit

---
+++ CoinMark/CoinMarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinMarkApp.swift
+//  CoinMark
+//
+//  Created by Emerson Day on 4/19/25.
+//
+
+import SwiftUI
+
+@main
+struct CoinMarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
